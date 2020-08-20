5673766d3fd9e786345de3064af476a8c847fe26
Andrei Telehoi <andrei.telehoi.99@gmail.com>
2020-08-20 13:31:44 +0300

    v1.0

---
+++ script.js
@@ -0,0 +1,22 @@
+let h3 = document.querySelector("h3");
+let color1 = document.querySelector(".color1");
+let color2 = document.querySelector(".color2");
+let colors = [color1, color2];
+
+let body = document.getElementById("gradient");
+
+function setGradient() {
+    body.style.background =
+        "linear-gradient(to right, "
+        + colors[0].value
+        + ", "
+        + colors[1].value
+        + ")";
+    h3.textContent = body.style.background + ";";
+}
+
+for (let color of colors) {
+    color.addEventListener("input", setGradient);
+}
+
+
