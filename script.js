adb17e73b5eb7167641e5c6492f4bbb5eb6cf49b
Andrei Telehoi <andrei.telehoi.99@gmail.com>
2020-08-20 22:56:23 +0300

    v1.0.3

--- script.js
+++ script.js
@@ -2,22 +2,19 @@ let h3 = document.querySelector("h3");
 let color1 = document.querySelector(".color1");
 let color2 = document.querySelector(".color2");
 let colors = [color1, color2];
-
 let body = document.getElementById("gradient");
 
-const setGradient = () => {
-    body.style.background =
-        "linear-gradient(to right, "
-        + colors[0].value
-        + ", "
-        + colors[1].value
-        + ")";
-    h3.textContent = body.style.background + ";";
-};
-
-for (let color of colors) {
-    color.addEventListener("input", setGradient);
-}
+colors.forEach((color) => {
+    color.addEventListener("input", () => {
+        body.style.background =
+            "linear-gradient(to right, "
+            + colors[0].value
+            + ", "
+            + colors[1].value
+            + ")";
+        h3.textContent = body.style.background + ";";
+    });
+})
 
 //just some random stuff for github purposes
 //just some random stuff for github purposes
